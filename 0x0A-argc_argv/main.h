5fe840070f7435a9a06c0cdd56087cf56fe27438
CNPaul <chinedungene3@gmail.com>
2022-06-24 12:15:12 -0700

    Great

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,5 +3,4 @@ int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
-int coinConverter(int i);
 int main(int argc, char *argv[]);
