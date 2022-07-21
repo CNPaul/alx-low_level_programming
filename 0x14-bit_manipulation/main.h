71feb767f984e28e1ad47909026b75c500367936
CNPaul <chinedungene3@gmail.com>
2022-07-21 09:10:49 -0700

    Cool

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,6 +6,8 @@
  *       written in the 0x13-bit_manipulation directory.
  */
 
+#define BYTE_LENGHT 8
+
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
